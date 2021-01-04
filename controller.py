ec8be32b1f05ed4b9472717177ea595809d488e8
mohamedkamalll <58629267+mohamedkamalll@users.noreply.github.com>
2021-01-04 20:15:25 +0200

    -

--- controller.py
+++ controller.py
@@ -44,18 +44,4 @@ class Controller:
         self.root.mainloop()
 
         
-    def reset(self):
-        self.xo = XO()
-        self.numOfClicks = 0
-        for i in range(9):
-            self.buttonList[i]["state"] = NORMAL
-            self.symbolTexts[i].set("")
-        self.buttonList[0]['command'] = lambda: self.click(0)
-        self.buttonList[1]['command'] = lambda: self.click(1)
-        self.buttonList[2]['command'] = lambda: self.click(2)
-        self.buttonList[3]['command'] = lambda: self.click(3)
-        self.buttonList[4]['command'] = lambda: self.click(4)
-        self.buttonList[5]['command'] = lambda: self.click(5)
-        self.buttonList[6]['command'] = lambda: self.click(6)
-        self.buttonList[7]['command'] = lambda: self.click(7)
-        self.buttonList[8]['command'] = lambda: self.click(8)
+
