19cfe811bc2922af253a47ee290b8747668c7952
Mostafa20180606 <mostafa_20180606@fci.helwan.edu.eg>
2021-01-04 02:17:50 +0200

    Add IsWinner Function

--- XO.py
+++ XO.py
@@ -13,7 +13,22 @@ class XO:
         else:
             return False
 
-    
+    def isWinner(self, symbol):
+        # check the row s
+        for i in range(0, 9, 3):
+            if self.board[i] == symbol and self.board[i + 1] == symbol and self.board[i + 2] == symbol:
+                return True
+        # check the columns
+        for i in range(0, 3):
+            if self.board[i] == symbol and self.board[i + 3] == symbol and self.board[i + 6] == symbol:
+                return True
+        # check from left to right diagonal
+        if self.board[0] == symbol and self.board[4] == symbol and self.board[8] == symbol:
+            return True
+        # check from right to left diagonal
+        if self.board[2] == symbol and self.board[4] == symbol and self.board[6] == symbol:
+            return True
+        return False
 
     def isTie(self):
         if not self.isWinner(self.symbols[0]) and not self.isWinner(self.symbols[1]):
